8c908c80fb0409a5e8101abc356151775a8da6cf
Nathan Kawaller <kawaller@gmail.com>
2021-02-01 11:18:47 -0500

    refactored test for two_sum

--- algorithms/arrays/two_sum.py
+++ algorithms/arrays/two_sum.py
@@ -15,16 +15,14 @@ Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 """
 
-def two_sum_brute_force(arr, target):
+def two_sum(arr, target):
     for i in range(0,len(arr)-1):
         for j in range(1,len(arr)):
             if arr[i] + arr[j] == target:
                 return [i,j]
     return 'No match'
 
+a = [1,2,3,4]
 
-if __name__ == '__main__':
-
-    a = [1,2,3,4]
-    print(two_sum_brute_force(a,7))
-    print(two_sum_brute_force(a,8))
+print(two_sum(a,7))
+print(two_sum(a,8))
