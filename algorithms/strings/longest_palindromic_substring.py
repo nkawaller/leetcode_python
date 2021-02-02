88076736134d7ca1036f873c6772c1782171e686
Nathan Kawaller <kawaller@gmail.com>
2021-02-02 07:56:46 -0500

    finished code and added description

--- algorithms/strings/longest_palindromic_substring.py
+++ algorithms/strings/longest_palindromic_substring.py
@@ -1,24 +1,36 @@
-def longestPalindromic(s):
-    res = ""
-    resLen = 0
-        
+"""
+Given a string s, return the longest palindromic substring in s.
+
+ 
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+"""
+
+
+def longestPalindrome(s: str) -> str:
+    result = ""
+    result_length = 0
+
     for i in range(len(s)):
-        # odd length
-        l,r = i, i
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            if (r-l+1) > resLen:
-                res = s[l:r+1]
-                resLen = r - l + 1
-            l -= 1
-            r += 1
-            
-        # even length
-        l,r = i, i + 1
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            if (r-l+1) > resLen:
-                res = s[l:r+1]
-                resLen = r-l+1
-            l -= 1
-            r += 1
-            
-    return res
+        left, right = i, i
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            if (right - left + 1) > result_length:
+                result = s[left:right+1]
+                result_length = right - left + 1
+
+            left -= 1
+            right += 1
+
+        left, right = i, i+1
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            if (right - left + 1) > result_length:
+                result = s[left:right+1]
+                result_length = right - left + 1
+
+            left -= 1
+            right += 1
+
+    return result
