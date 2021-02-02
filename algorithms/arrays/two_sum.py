63b7d672db2388575fef13e95466a6e3f9a00165
Nathan Kawaller <kawaller@gmail.com>
2021-02-02 10:18:34 -0500

    renamed file

--- algorithms/arrays/two_sum.py
+++ algorithms/arrays/two_sum.py
@@ -15,14 +15,28 @@ Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 """
 
-def two_sum(arr, target):
+def two_sum_brute_force(arr, target):
     for i in range(0,len(arr)-1):
         for j in range(1,len(arr)):
             if arr[i] + arr[j] == target:
                 return [i,j]
     return 'No match'
 
-a = [1,2,3,4]
 
-print(two_sum(a,7))
-print(two_sum(a,8))
+def two_sum_hash(arr, target):
+    dictionary = {}
+    for index, value in enumerate(arr):
+        if (target-value) in dictionary:
+            return [dictionary[target-value], index]
+        else:
+            dictionary[value] = index
+
+
+if __name__ == '__main__':
+
+    a = [1,2,3,4]
+    print(two_sum_brute_force(a,7))
+    print(two_sum_brute_force(a,8))
+    print(two_sum_hash(a,3))
+    print(two_sum_hash(a,7))
+    print(two_sum_hash(a,8))
