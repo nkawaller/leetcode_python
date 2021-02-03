12193f368cb13f08d5fa9bb313e97f9032cf66d7
Nathan Kawaller <kawaller@gmail.com>
2021-02-03 09:35:01 -0500

    reverse linked list solution

---
+++ algorithms/linked_list/reverse_linked_list.py
@@ -0,0 +1,21 @@
+"""
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        while head:
+            head.next, prev, head = prev, head, head.next
+        return prev
