6e4113a4331bf5eac4d0439bdd8c4c12abd57a32
Nathan Kawaller <kawaller@gmail.com>
2021-02-03 09:44:23 -0500

    fixed linting errors

--- algorithms/linked_list/reverse_linked_list.py
+++ algorithms/linked_list/reverse_linked_list.py
@@ -9,10 +9,13 @@ Output: 5->4->3->2->1->NULL
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev = None
