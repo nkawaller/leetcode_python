38a55b9992d827059f705abed0eb1dadb6aeb774
Nathan Kawaller <kawaller@gmail.com>
2021-02-03 11:30:53 -0500

    commented out tests for now

--- algorithms/tests/test_linked_list.py
+++ algorithms/tests/test_linked_list.py
@@ -1,5 +1,9 @@
-import unittest
+# import unittest
 
-from algorithms.linked_list import (
-    reverse_linked_list
-)
+# from algorithms.linked_list import (
+#     reverse_linked_list
+# )
+
+
+# if __name__ == '__main__':
+#     unittest.main()
--- algorithms/tests/test_strings.py
+++ algorithms/tests/test_strings.py
@@ -1,13 +1,18 @@
-import unittest
+# import unittest
 
-from leetcode_python.algorithms.strings import valid_palindrome
+# from valid_palindrome import isPalindrome
 
 
-class TestIsPalindrome(unittest.TestCase):
-    """
-    Test for the file valid_palindrome.py
-    """
+# class TestIsPalindrome(unittest.TestCase):
+#     """
+#     Test for the file valid_palindrome.py
+#     """
 
-    def test_isPalindrome(self):
-        self.assertTrue(isPalindrome('racecar'))
-        self.assertFalse(isPalindrome('bass'))
+#     def test_isPalindrome(self):
+#         self.assertTrue(isPalindrome('racecar'))
+#         self.assertTrue(isPalindrome('A man a plan a canal: Panama'))
+#         self.assertFalse(isPalindrome('Race a car'))
+
+
+# if __name__ == '__main__':
+#     unittest.main()
