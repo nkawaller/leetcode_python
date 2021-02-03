3c2446a14e6859be05b7ea6263ad5af100f42522
Nathan Kawaller <kawaller@gmail.com>
2021-02-03 10:12:44 -0500

    split up if statement for faster runtime

--- algorithms/strings/valid_parentheses.py
+++ algorithms/strings/valid_parentheses.py
@@ -34,7 +34,8 @@ class Solution:
                 stack.append(i)
             elif i in close_list:
                 pos = close_list.index(i)
-                if len(stack) != 0 and open_list[pos] == stack[len(stack)-1]:
+                if ((len(stack) != 0) and 
+                    (open_list[pos] == stack[len(stack)-1])):
                     stack.pop()
                 else:
                     return False
