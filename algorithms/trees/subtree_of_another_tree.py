eb30fc7c4226560f08cde9fe75bdb15b7622723d
Nathan Kawaller <kawaller@gmail.com>
2021-02-05 20:37:21 -0500

    added second solution

--- algorithms/trees/subtree_of_another_tree.py
+++ algorithms/trees/subtree_of_another_tree.py
@@ -45,3 +45,29 @@ class Solution:
                self.areIdentical(r1.left, r2.left) and
                 self.areIdentical(r1.right, r2.right)
                )
+"""
+Alternate solution
+"""
+
+import collections
+
+def is_subtree(big, small):
+    flag = False
+    queue = collections.deque()
+    queue.append(big)
+    while queue:
+        node = queue.popleft()
+        if node.val == small.val:
+            flag = comp(node, small)
+            break
+        else:
+            queue.append(node.left)
+            queue.append(node.right)
+    return flag
+
+def comp(p, q):
+    if p is None and q is None:
+        return True
+    if p is not None and q is not None:
+        return p.val == q.val and comp(p.left,q.left) and comp(p.right,q.right)
+    return False
