dd3fdb5d631a47c91d50f8b381a533e4760a03d1
Nathan Kawaller <kawaller@gmail.com>
2021-02-08 11:23:29 -0500

    solution

--- algorithms/arrays/maximum_subarray.py
+++ algorithms/arrays/maximum_subarray.py
@@ -14,3 +14,13 @@ Example 2:
 Input: nums = [1]
 Output: 1
 """
+
+
+def maxSubArray(nums):
+
+    total_sum = max_sum = nums[0]
+
+    for i in nums[1:]:
+        total_sum = max(i, total_sum + i)
+        max_sum = max(total_sum, max_sum)
+    return max_sum
