9e16c897571882e87db98c72a0a8759e30b001d4
Nathan Kawaller <kawaller@gmail.com>
2021-03-06 07:18:58 -0500

    formatting

--- algorithms/strings/valid_parentheses.py
+++ algorithms/strings/valid_parentheses.py
@@ -24,18 +24,19 @@ Input: s = "(]"
 Output: false
 """
 
+
 class Solution:
     def isValid(self, s: str) -> bool:
-        open_list = ['[','{','(']
-        close_list = [']','}',')']
+        open_list = ['[', '{', '(']
+        close_list = [']', '}', ')']
         stack = []
         for i in s:
             if i in open_list:
                 stack.append(i)
             elif i in close_list:
                 pos = close_list.index(i)
-                if ((len(stack) != 0) and 
-                    (open_list[pos] == stack[len(stack)-1])):
+                if ((len(stack) != 0) and
+                        (open_list[pos] == stack[len(stack)-1])):
                     stack.pop()
                 else:
                     return False
